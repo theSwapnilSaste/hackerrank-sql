31b66e745894514332599fd93a84d601bfe5c7fa
theSwapnilSaste <theSwapnilSaste@gmail.com>
2020-01-25 02:06:17 +0530

    Added another 3 solutions in basic-select

---
+++ Basic-Select/Employee-Names.sql
@@ -0,0 +1,9 @@
+-- TITLE:       HackerRank / Employee Names
+-- LINK:        https://www.hackerrank.com/challenges/name-of-employees
+-- DESCRIPTION: Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+-- AUTHOR:      https://github.com/theSwapnilSaste/
+-- DATE:        25-JAN-2019
+-- ORACLE
+SELECT      name
+FROM        employee
+ORDER BY    name;
---
+++ Basic-Select/Higher-Than-75-Marks.sql
@@ -0,0 +1,10 @@
+-- TITLE:       HackerRank / Higher Than 75 Marks
+-- LINK:        https://www.hackerrank.com/challenges/more-than-75-marks/problem
+-- DESCRIPTION: Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+-- AUTHOR:      https://github.com/theSwapnilSaste/
+-- DATE:        25-JAN-2019
+-- ORACLE
+SELECT   name 
+FROM     students 
+WHERE    marks>75 
+ORDER BY SUBSTR(name,-3),id ;
